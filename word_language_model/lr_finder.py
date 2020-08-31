3e0edaf5144fa691b9d4cc5b593289b568f7fc1d
henryqin <henryqin@umich.edu>
2020-08-31 01:31:33 -0700

    lr finder

--- word_language_model/lr_finder.py
+++ word_language_model/lr_finder.py
@@ -140,8 +140,8 @@ optimizer = torch.optim.SGD(model.parameters(),lr=1,momentum=0.9)
 best_val_loss = None
 
 def lrs(batch):
-    low = math.log2(1e-5)
-    high = math.log2(100)
+    low = math.log2(1e-1)
+    high = math.log2(10000)
     return 2**(low+(high-low)*batch/train_data.size(0)/args.epochs)
 
 lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer,lrs)
