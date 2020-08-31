1c149b007b09f311737b3298ccd7e2f6ae6736f5
henryqin <henryqin@umich.edu>
2020-08-31 02:34:33 -0700

    lr finder

--- word_language_model/lr_finder.py
+++ word_language_model/lr_finder.py
@@ -201,7 +201,7 @@ def train():
             elapsed = time.time() - start_time
             print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
                     'loss {:5.2f} | ppl {:8.2f}'.format(
-                epoch, batch, len(train_data) // args.bptt, lr_scheduler.get_lr(),
+                epoch, batch, len(train_data) // args.bptt, float(lr_scheduler.get_last_lr()),
                 elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
             total_loss = 0
             start_time = time.time()
