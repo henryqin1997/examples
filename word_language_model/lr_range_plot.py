970667a93a6fec13dac429810e5eedb3fa1c033d
henryqin <henryqin@umich.edu>
2020-09-02 19:21:15 -0700

    warmup

--- word_language_model/lr_range_plot.py
+++ word_language_model/lr_range_plot.py
@@ -2,18 +2,17 @@ import json
 import math
 from matplotlib import pyplot as plt
 
-low = math.log2(1e-2)
-high = math.log2(100)
-point = math.log2(13)
+low = math.log2(1e-4)
+high = math.log2(2)
 
 loss = json.load(open('/Users/qin/workplace/research/examples/word_language_model/wikitext-2_lstm_lr_range_find.json'))
 
-x = [2**(low+(high-low)*i/len(loss)) for i in range(int(len(loss)*(point-low)/(high-low)))]
+x = [2**(low+(high-low)*i/len(loss)) for i in range(int(len(loss)*(high-low)/(high-low)))]
 i = loss.index(min(loss))
 print(i,2**(low+(high-low)*i/len(loss)))
-# loss = loss[:int(len(loss)*(point-low)/(high-low))]
-# plt.plot(x,loss)
-# plt.xscale('log')
-# plt.ylabel('loss')
-# plt.xlabel('lr')
-# plt.show()
+loss = loss[:int(len(loss)*(high-low)/(high-low))]
+plt.plot(x,loss)
+plt.xscale('log')
+plt.ylabel('loss')
+plt.xlabel('lr')
+plt.show()
